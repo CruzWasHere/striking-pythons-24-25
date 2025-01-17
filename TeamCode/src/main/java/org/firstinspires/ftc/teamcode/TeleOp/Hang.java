a2a5fb4ccf59688319dc108d153adf835f0dd68d
cruzwashere <renatoykike@gmail.com>
2025-01-16 22:50:02 -0500

    Auto Updates Done for FTC Meet #3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Hang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Hang.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.TeleOp.RobotHardware;
-
-
 public class Hang {
 
     public final RobotHardware robot;
@@ -17,12 +14,25 @@ public class Hang {
 
     // Hang Programming
 
-    public void controlHang(boolean dpad_down, boolean dpad_up) {
-        if (dpad_down) {
-            setHangPower(1, 100); // Hang Down
-        } else if (dpad_up) {
-            setHangPower(-1, 100); // Hang Up
+    public void controlHang(boolean buttonA, boolean buttonY, boolean leftBumper, boolean rightBumper, int leftStick) {
+        if (buttonA) {
+            setClawPower(1, 100); // Hang Down
+        } else if (buttonY) {
+            setClawPower(-1, 100); // Hang Up
         }
+        if (leftBumper) {
+            setLeftClawServo(1);
+            setRightClawServo(0);
+        } else if (rightBumper) {
+            setLeftClawServo(0.5);
+            setRightClawServo(0.7);
+        }
+        if (leftStick > 0) {
+            setHangPower(1,100);
+        } else if (leftStick < 0) {
+            setHangPower(-1, 100);
+        }
+
     }
 
     public void setHangPower(double power, int time) {
@@ -31,5 +41,17 @@ public class Hang {
         opMode.sleep(time);
         robot.leftHang.setPower(0);
         robot.rightHang.setPower(0);
+    }
+    public void setLeftClawServo ( double Pos){
+
+        robot.leftClawServo.setPosition(Pos);
+    }
+    public void setRightClawServo (double Pos){
+        robot.rightClawServo.setPosition(Pos);
+    }
+    public void setClawPower ( double power, int time) {
+        robot.Claw.setPower(power);
+        opMode.sleep(time);
+        robot.Claw.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotHardware.java
@@ -9,7 +9,7 @@ public class RobotHardware {
 
     // Declare motors and servos
     public DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
-    public DcMotor leftClaw, rightClaw, leftHang, rightHang;
+    public DcMotor Claw, leftHang, rightHang;
     public Servo leftRotation, rightRotation, rightClawServo, leftClawServo;
 
     // Initialization method to set up all hardware
@@ -20,21 +20,19 @@ public class RobotHardware {
         frontRightMotor = hardwareMap.get(DcMotor.class, "rightFront");
         backRightMotor = hardwareMap.get(DcMotor.class, "rightBack");
 
-        leftClaw = hardwareMap.get(DcMotor.class, "leftClaw");
-        rightClaw = hardwareMap.get(DcMotor.class, "rightClaw");
+        Claw = hardwareMap.get(DcMotor.class, "Claw");
 
         leftHang = hardwareMap.get(DcMotor.class, "leftHang");
         rightHang = hardwareMap.get(DcMotor.class, "rightHang");
 
-        leftRotation = hardwareMap.get(Servo.class, "leftRotation");
-        rightRotation = hardwareMap.get(Servo.class, "rightRotation");
+        leftRotation = hardwareMap.servo.get("leftRotation");
+        rightRotation = hardwareMap.servo.get("rightRotation");
         rightClawServo = hardwareMap.get(Servo.class, "rightClawServo");
         leftClawServo = hardwareMap.get(Servo.class, "leftClawServo");
 
         // Configure motor directions if needed
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftClaw.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set zero power behavior for motors
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
