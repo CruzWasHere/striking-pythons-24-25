f095eeb96ecb609b079d634acda04edc8029c9f1
adnytoony <ariadnammonzon@gmail.com>
2025-01-17 15:34:17 -0500

    fixConflicts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ControlsNEW.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ControlsNEW extends LinearOpMode {
+    public final RobotHardware robot = new RobotHardware();
+    public Base driveControl;
+
+    public Hang hangControl;
+
+
+    @Override
+
+    public void runOpMode() {
+        robot.init(hardwareMap); // Initialize robot hardware
+        driveControl = new Base(robot); // Initialize Control1 with RobotHardware
+        hangControl = new Hang(robot, this); // Initialize Hang with RobotHardware
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            // Pass the joystick values to Control1's drive method
+            double y = -gamepad1.left_stick_y; // Forward/backward
+            double x = gamepad1.left_stick_x * 1.1; // Strafing
+            double rx = gamepad1.right_stick_x; // Rotation
+            driveControl.drive(y, x, rx);
+
+            hangControl.controlHang(
+                    gamepad2.a,
+                    gamepad2.y,
+                    gamepad2.left_bumper,
+                    gamepad2.right_bumper,
+                    (int) Math.signum (gamepad2.left_stick_y)
+            );
+        }
+    }
+}
